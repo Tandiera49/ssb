8684401b6fc26f69a522a16d9721368c5395564a
Tandiera49 <yostandi2@gmail.com>
2026-09-12 23:18:27 +0700

    Prepare Cloudflare deployment dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
